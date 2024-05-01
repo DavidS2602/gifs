13e72d66a6dfe79a511b28c82ecc36b04e9a70af
David <DavidS260202@proton.me>
2024-05-01 15:52:43 -0600

    Add http request to Giphy api with HttpClient

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,16 @@
+// biome-ignore lint/style/useImportType: <explanation>
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+  private serviceUrl = 'https://api.giphy.com/v1/gifs'
+  private apiKey = '4UseQUMqwyDu07Pmh7hZTn5uKmK4srUR'
+
+  constructor(private http: HttpClient) { }
+
   private _tagsHistory: string[] = []
 
   get tagsHistory() {
@@ -11,18 +18,26 @@ export class GifsService {
   }
 
   private organizeHistory(tag: string) {
-    tag = tag.toLowerCase()
-    if (this._tagsHistory.includes(tag)) {
-      this._tagsHistory = this._tagsHistory.filter(t => t !== tag)
+    const lowercaseTag = tag.toLowerCase();
+    if (this._tagsHistory.includes(lowercaseTag)) {
+      this._tagsHistory = this._tagsHistory.filter(t => t !== lowercaseTag);
     }
-    this._tagsHistory.unshift(tag) // Add tag to the beginning of the array
-    this._tagsHistory = this._tagsHistory.splice(0, 10) // Keep only the last 10 tags
+    this._tagsHistory.unshift(lowercaseTag); // Add tag to the beginning of the array
+    this._tagsHistory = this._tagsHistory.splice(0, 10); // Keep only the last 10 tags
   }
 
-
   searchTag(tag: string): void {
-    if (tag.length === 0) return
-    this.organizeHistory(tag)
-    console.log(this._tagsHistory)
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams() // Create a new HttpParams object
+      .set('api_key', this.apiKey)
+      .set('q', tag)
+      .set('limit', '10');
+
+    this.http.get(`${this.serviceUrl}/search?`,{params}) // Use the params object in the request
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 }
